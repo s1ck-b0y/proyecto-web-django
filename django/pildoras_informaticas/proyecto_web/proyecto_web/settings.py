8573f76ee1132fa27dc56f288ff1809d58bf52cc
Cosme <aurea-mediocritas@hotmail.com>
2022-02-27 21:41:56 -0300

    Migrated to PostgreSQL

--- django/pildoras_informaticas/proyecto_web/proyecto_web/settings.py
+++ django/pildoras_informaticas/proyecto_web/proyecto_web/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'proyecto_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'proyecto-web',
+        'USER': 'mesa20',
+        'PASSWORD': 'mesa20',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
     }
 }
 
