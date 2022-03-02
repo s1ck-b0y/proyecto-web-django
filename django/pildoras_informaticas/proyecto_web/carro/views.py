bfbb125118c9205800df6b1772a1a1f19854f333
s1ck-b0y <maxi.barreneche@gmail.com>
2022-03-02 14:53:05 -0300

    Added Store Views and set Shopping Cart location

--- django/pildoras_informaticas/proyecto_web/carro/views.py
+++ django/pildoras_informaticas/proyecto_web/carro/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .carro import Carro
+
+from tienda.models import Producto
 
 # Create your views here.
+
+def agregarProducto(request,producto_id):
+    carro=Carro(request)
+    producto=Producto.objects.get(id=producto_id)
+    carro.agregar(producto=producto)
+    return redirect("tienda")
+
+def eliminarProducto(request,producto_id):
+    carro=Carro(request)
+    producto=Producto.objects.get(id=producto_id)
+    carro.eliminar(producto=producto)
+    return redirect("tienda")
+
+def restarProducto(request,producto_id):
+    carro=Carro(request)
+    producto=Producto.objects.get(id=producto_id)
+    carro.restarProducto(producto=producto)
+    return redirect("tienda")
+
+def limpiarCarro(request):
+    carro=Carro(request)
+    carro.limpiarCarro()
+    return redirect("tienda")
